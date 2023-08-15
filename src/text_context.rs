7053e60a3a8e8dbcbe376b669b0f1b1741aed884
Matthieu Casanova <public@kpouer.com>
2023-08-15 10:52:56 +0200

    use derive

--- src/text_context.rs
+++ src/text_context.rs
@@ -1,17 +1,8 @@
 use std::collections::BTreeMap;
 
+#[derive(Default)]
 pub struct Context {
     pub text: String,
     pub current_encoding: Option<String>,
     pub encodings: BTreeMap<Vec<u8>, String>,
-}
-
-impl Default for Context {
-    fn default() -> Self {
-        Self {
-            text: String::new(),
-            current_encoding: None,
-            encodings: BTreeMap::new(),
-        }
-    }
 }
